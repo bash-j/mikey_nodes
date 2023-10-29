b99e26f72b239664a8a158474b4317979117278b
bash-j <jonv@fastmail.com>
2023-10-29 15:57:00 +1030

    modified:   mikey_nodes.py

--- mikey_nodes.py
+++ mikey_nodes.py
@@ -3718,6 +3718,53 @@ class OobaPrompt:
         prompt.get(str(unique_id))['inputs']['output_text'] = result
         return (result,)
 
+class WildcardOobaPrompt:
+    # processes wildcard syntax
+    # and also processes a llm sytax using the oobaprompt class
+    @classmethod
+    def INPUT_TYPES(s):
+        return {'required': {'input_prompt': ('STRING', {'multiline': True, 'default': 'Prompt Text Here', 'dynamicPrompts': False}),
+                             #'mode': (['prompt', 'style', 'descriptor', 'custom'], {'default': 'prompt'}),
+                             #'custom_history': ('STRING', {'multiline': False, 'default': 'path to history.json', 'dynamicPrompts': True}),
+                             'seed': ('INT', {'default': 0, 'min': 0, 'max': 0xffffffffffffffff})},
+                "hidden": {"unique_id": "UNIQUE_ID", "extra_pnginfo": "EXTRA_PNGINFO", "prompt": "PROMPT"}}
+
+    RETURN_TYPES = ('STRING',)
+    RETURN_NAMES = ('text',)
+    FUNCTION = 'process'
+    OUTPUT_NODE = True
+    CATEGORY = 'Mikey/AI'
+
+    def process(self, input_prompt, seed, prompt=None, unique_id=None, extra_pnginfo=None):
+        # search and replace
+        input_prompt = search_and_replace(input_prompt, extra_pnginfo, prompt)
+        # wildcard sytax is {like|this}
+        # select a random word from the | separated list
+        wc_re = re.compile(r'{([^}]+)}')
+        def repl(m):
+            return random.choice(m.group(1).split('|'))
+        for m in wc_re.finditer(input_prompt):
+            input_prompt = input_prompt.replace(m.group(0), repl(m))
+        # process wildcards
+        input_prompt = find_and_replace_wildcards(input_prompt, seed, debug=True)
+        # check if llm syntax is in the prompt
+        # examples <llm:prompt:prompt text here>, <llm:style:prompt text here>, <llm:descriptor:prompt text here>
+        # example for custom history file <llm:path/to/history.json:prompt text here>
+        llm_re = re.compile(r'<llm:(.*?):(.*?)>')
+        for m in llm_re.finditer(input_prompt):
+            mode = m.group(1)
+            if '.json' in mode:
+                custom_history = mode
+                mode = 'custom'
+            else:
+                custom_history = None
+            prompt_text = m.group(2)
+            ooba = OobaPrompt()
+            result = ooba.api_request(prompt_text, seed, mode, custom_history)
+            input_prompt = input_prompt.replace(m.group(0), result)
+        prompt.get(str(unique_id))['inputs']['output_text'] = input_prompt
+        return (input_prompt,)
+
 NODE_CLASS_MAPPINGS = {
     'Wildcard Processor': WildcardProcessor,
     'Empty Latent Ratio Select SDXL': EmptyLatentRatioSelector,
@@ -3766,7 +3813,8 @@ NODE_CLASS_MAPPINGS = {
     'Text2InputOr3rdOption': Text2InputOr3rdOption,
     'Checkpoint Loader Simple Mikey': CheckpointLoaderSimpleMikey,
     'TextPreserve': TextPreserve,
-    'OobaPrompt': OobaPrompt
+    'OobaPrompt': OobaPrompt,
+    'WildcardOobaPrompt': WildcardOobaPrompt
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -3817,5 +3865,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     'Text2InputOr3rdOption': 'Text 2 Inputs Or 3rd Option Instead (Mikey)',
     'Checkpoint Loader Simple Mikey': 'Checkpoint Loader Simple (Mikey)',
     'TextPreserve': 'Text Preserve (Mikey)',
-    'OobaPrompt': 'OobaPrompt (Mikey)'
+    'OobaPrompt': 'OobaPrompt (Mikey)',
+    'WildcardOobaPrompt': 'Wildcard OobaPrompt (Mikey)'
 }
